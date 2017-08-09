ac3955313e3ce63c549ff5ad12a839a4d097b9c4
Juan Garcia <jggarcia@sfu.ca>
2017-08-09 00:50:20 -0700

    ready to run

--- utility/preprocessdesign.m
+++ utility/preprocessdesign.m
@@ -5,7 +5,7 @@ clc
 close all
 
 
-Design = importdata('design64points.txt')
+Design = importdata('design.txt');
 
 gamma_vec = Design(:,1);
 z_0_vec = Design(:,2);
@@ -28,10 +28,10 @@ alphay_vec = 0.1*(z_i.^(3/4)).*((-0.4.*L_vec).^(-1/3)).*u_star;
 alphax_vec = alphay_vec;
 
 % write files 
-dlmwrite('alphashear.dat', gamma_vec)
-dlmwrite('z_0.dat', z_0_vec)
-dlmwrite('u_cutoff.dat', z_cutoff_vec)
-dlmwrite('diffL.dat', L_vec)
-dlmwrite('alphaz.dat', alphaz_vec)
-dlmwrite('alphax.dat', alphax_vec)
-dlmwrite('alphay.dat', alphay_vec)
+dlmwrite('../alphashear.dat', gamma_vec)
+dlmwrite('../z_0.dat', z_0_vec)
+dlmwrite('../u_cutoff.dat', z_cutoff_vec)
+dlmwrite('../diffL.dat', L_vec)
+dlmwrite('../alphaz.dat', alphaz_vec)
+dlmwrite('../alphax.dat', alphax_vec)
+dlmwrite('../alphay.dat', alphay_vec)
